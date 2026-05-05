e5d8be244c1d33a0f936d4342f4bd854d487f04e
VictorConrado <estudantecomv@gmail.com>
2026-05-05 14:14:30 -0300

    chore: Primeiro commit contendo o primeiro teste referente à Palíndromos

---
+++ codenapp-test/Desafios/DesafioPalindromo/Palindromo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codenapp_test.Desafios.DesafioPalindromo
+{
+    public class Palindromo
+    {
+        public static bool ValidaPalindromo(string entrada)
+        {
+            var LimpaTexto = new System.Text.StringBuilder();
+            foreach (char caractere in entrada)
+                if (char.IsLetter(caractere))
+                    LimpaTexto.Append(char.ToLower(caractere));
+
+            string textoLimpo = LimpaTexto.ToString();
+
+            int esquerda = 0;
+            int direita = textoLimpo.Length - 1;
+
+            while (esquerda < direita)
+            {
+                if (textoLimpo[esquerda] != textoLimpo[direita])
+                    return false;
+                esquerda++;
+                direita--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ codenapp-test/Program.cs
@@ -0,0 +1,9 @@
+﻿using codenapp_test.Desafios.DesafioPalindromo;
+
+var testes = new[]
+{
+    "Arara", "Ovo", "Roma me tem amor", "O lobo ama o bolo", "Hello"
+};
+
+foreach (var t in testes)
+    Console.WriteLine($"\"{t}\" -> {(Palindromo.ValidaPalindromo(t) ? "É PALÍNDROMO V" : "NÃO É PALÍNDROMO X")}");
