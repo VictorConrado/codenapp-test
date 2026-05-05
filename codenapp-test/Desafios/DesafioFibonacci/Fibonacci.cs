af0161f029e772e4110c78426a68e29c0313a565
VictorConrado <estudantecomv@gmail.com>
2026-05-05 14:47:45 -0300

    feat: Incluindo resolução do desafio da sequencia de Fibonacci

---
+++ codenapp-test/Desafios/DesafioFibonacci/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codenapp_test.Desafios.DesafioFibonacci
+{
+    public class Fibonacci
+    {
+        public static List<int> CalculaFibonacci(int x)
+        {
+            List<int> SequenciaFibonacci = new List<int>();
+
+            if (x <= 0) return SequenciaFibonacci;
+
+            SequenciaFibonacci.Add(0);
+            if (x == 1) return SequenciaFibonacci;
+
+            SequenciaFibonacci.Add(1);
+
+            for (int i = 2; i < x; i++)
+            {
+                int proximoValor = SequenciaFibonacci[i - 1] + SequenciaFibonacci[i - 2];
+                SequenciaFibonacci.Add(proximoValor);
+            }
+
+            return SequenciaFibonacci;
+        }
+    }
+}
--- codenapp-test/Program.cs
+++ codenapp-test/Program.cs
@@ -1,5 +1,9 @@
-﻿using codenapp_test.Desafios.DesafioPalindromo;
+﻿using codenapp_test.Desafios.DesafioFibonacci;
+using codenapp_test.Desafios.DesafioPalindromo;
 
+// Palíndromo 
+
+/*
 var testes = new[]
 {
     "Arara", "Ovo", "Roma me tem amor", "O lobo ama o bolo", "Hello"
@@ -7,3 +11,14 @@ var testes = new[]
 
 foreach (var t in testes)
     Console.WriteLine($"\"{t}\" -> {(Palindromo.ValidaPalindromo(t) ? "É PALÍNDROMO V" : "NÃO É PALÍNDROMO X")}");
+*/
+
+// Fibonacci
+
+int[] testes = { 3, 5, 7 };
+
+foreach (int x in testes)
+{
+    List<int> result = Fibonacci.CalculaFibonacci(x);
+    Console.WriteLine($"Quando X = {x} o resultado é {string.Join(", ", result)}");
+}
